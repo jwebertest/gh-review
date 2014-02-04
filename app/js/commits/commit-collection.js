e51079381e62dd338161e3a6ece8109d40c65e58
Jörg Weber <weber.joerg.h@gmail.com>
2014-02-04 23:56:16 +0100

    * remove localStorage support for collections and comments

--- app/js/commits/commit-collection.js
+++ app/js/commits/commit-collection.js
@@ -3,7 +3,6 @@ define(['backbone', 'commitModel'], function (Backbone, CommitModel) {
   'use strict';
   var CommitsCollection = Backbone.Collection.extend({
     model: CommitModel,
-    localStorage: new Backbone.LocalStorage('commits'),
     initialize: function () {
 
     }
