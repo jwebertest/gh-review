ee72c5c7129e37acdbf19e524898dcf2090fb2fb
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-03 20:20:36 +0100

    * get all commits until the last 2 weeks to show statistics

--- app/js/statistics/statistic-model.js
+++ app/js/statistics/statistic-model.js
@@ -4,22 +4,47 @@ define(['backbone', 'when', 'underscore', 'app', 'moment'], function (Backbone, 
 
   return Backbone.Model.extend({
     defaults: {
-      since: moment().subtract('weeks', 2)
+      since: moment().subtract('weeks', 2).toISOString(),
+      commits: [],
+      commentedCommits: []
+    },
+    getCommitDefer: when.defer(),
+    nextPage: true,
+    getCommits: function (link) {
+      var callback = function (error, resp) {
+        if (!error) {
+          var link = resp.meta.link;
+          var hasNext = app.github.hasNextPage(link);
+          var commits = this.get('commits');
+          delete resp.meta;
+          var concatenatedArray = commits.concat(resp);
+          this.set('commits', concatenatedArray);
+          if (hasNext) {
+            this.getCommits(link);
+          } else {
+            this.getCommitDefer.resolve();
+          }
+        }
+      }.bind(this);
+      if (!link) {
+        app.github.repos.getCommits({
+          user: this.get('owner'),
+          repo: this.get('repo'),
+          branch: this.get('branch'),
+          since: this.get('since'),
+          'per_page': 100
+        }, callback);
+      } else {
+        app.github.getNextPage(link, callback);
+      }
+      return this.getCommitDefer.promise;
     },
     computeStatistics: function () {
       var defer = when.defer();
-      app.github.repos.getCommits({
-        user: this.get('owner'),
-        repo: this.get('repo'),
-        branch: this.get('branch'),
-        since: this.get('since'),
-        'per_page': 100
-      }, function (error, resp) {
-        if (!error) {
-          console.log(resp);
+      this.getCommits()
+        .then(function () {
           defer.resolve();
-        }
-      });
+        }.bind(this));
       return defer.promise;
     }
   });
