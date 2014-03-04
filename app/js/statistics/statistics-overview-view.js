f916d0d2147f336895809d0c7e8bd262e5bca13d
Jörg Weber <weber.joerg.h@gmail.com>
2014-03-04 17:59:52 +0100

    * remove unused functions and events * remove console.log statement

--- app/js/statistics/statistics-overview-view.js
+++ app/js/statistics/statistics-overview-view.js
@@ -10,15 +10,7 @@ define([
   return Backbone.View.extend({
     el: '#main',
     template: _.template(template),
-    events: {
-      'click li': 'computeStatistic'
-    },
-    computeStatistic: function(event){
-      var target = this.$(event.target);
-      console.log(target);
-    },
     render: function(){
-      console.log(app.filterCollection.toJSON());
       this.$el.html(this.template({models: app.filterCollection.toJSON()}));
     }
   });
